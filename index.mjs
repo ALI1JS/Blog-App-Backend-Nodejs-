7125e52d1f5e9abc3853ebe73b1b0bdcbff50121
ALI.JS <aliismailh08@gmail.com>
2023-11-16 22:06:32 +0200

    updates for vercel

--- index.mjs
+++ index.mjs
@@ -1,34 +1,67 @@
-import {router as authRouter} from "./src/routes/userauth.mjs";
-import {router as articlRouter} from "./src/routes/articlerouter.mjs";
-import {router as commentRouter} from "./src/routes/commentsrouter.mjs";
-import {router as replyRouter} from "./src/routes/replyrouter.mjs";
-import {router as paymentRouter} from "./src/routes/paymentrouter.mjs";
-import { router as actionRouter}  from "./src/routes/articleActionrouter.mjs";
-import { router as userActionRouter}  from "./src/routes/useractionrouter.mjs";
-import ConnectToDataBase from "./config/databaseConnection.mjs";
-import { connect } from "mongoose";
-import dotenv from "dotenv";
-import cors from "cors";
-import swaggerUi from "swagger-ui-express";
-import swaggerDocument from "./swagger.json"  assert {type: "json"};
-import {router as searchRouter} from "./src/routes/searchrouter.mjs";
-import {router as adminRouter} from "./src/routes/adminrouter.mjs";
+import express from "express";
+const app = express();
+
+/**
+ * Import routes and Required functions from one file
+ * To achieve the Readablity
+ */
+
+import {
+cors,
+dotenv,
+swaggerUi,
+swaggerDocument,
+userActionRouter,
+authRouter,
+articlRouter,
+commentRouter,
+replyRouter,
+actionRouter,
+paymentRouter,
+adminRouter,
+ConnectToDataBase,
+searchRouter
+} from "./index2.mjs"
+
+/***
+ * App Settings
+ */
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+dotenv.config();
+app.use("/api-docs",swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+/***
+ * Database Connection
+ */
+
+ConnectToDataBase();
 
 
-export {
-    connect,
-    dotenv,
-    cors,
-    authRouter,
-    articlRouter,
-    commentRouter,
-    replyRouter,
-    paymentRouter,
-    actionRouter,
-    userActionRouter,
-    searchRouter,
-    adminRouter,
-    ConnectToDataBase,
-    swaggerUi,
-    swaggerDocument
-}
+/***
+ * routes
+ */
+
+
+app.use("/api", authRouter);
+app.use("/api", articlRouter);
+app.use("/api", commentRouter);
+app.use("/api", replyRouter);
+app.use("/api", paymentRouter);
+app.use("/api", actionRouter);
+app.use("/api", userActionRouter);
+app.use("/api", searchRouter);
+app.use("/api", adminRouter);
+app.use("*", (req, res)=>{
+   return res.json({message:"Thia EndPoint Not Found"});  
+})
+/***
+ * Listening
+ */
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+     console.log(`listening on port ${PORT}`);
+})
